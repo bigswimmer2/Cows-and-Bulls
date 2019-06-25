a85ea83194a6a0fb7a9b386dfd22b1ebec00991d
Brett Pinfold <brettpinfold2@gmail.com>
2019-06-24 20:36:09 -0500

    Finished the tic tac toe game

--- Tic Tac Toe.py
+++ Tic Tac Toe.py
@@ -1,8 +1,7 @@
 from array import *
 
-global board, count, game_running, turn_count, player, correct
+global board, game_running, turn_count, player, correct
 board = [['*','*','*'], ['*','*','*'], ['*','*','*']]
-count = 0
 game_running = True
 player = 1
 running = True
@@ -30,29 +29,56 @@ def reset_board():
     correct = False
 
 
-def player_turn(player_turn):
-    if player_turn == 1:
+def player_turn(turn):
+    if turn == 1:
         return "X"
     else:
         return "O"
 
 
+def looking_at_the_board():
+    if board[0][0] == board[0][1] and board[0][1] == board[0][2]:
+        return board[0][0]
+    elif board[1][0] == board[1][1] and board[1][1] == board[1][2]:
+        return board[1][0]
+    elif board[2][0] == board[2][1] and board[2][1] == board[2][2]:
+        return board[2][0]
+    elif board[0][0] == board[1][0] and board[1][0] == board[2][0]:
+        return board[0][0]
+    elif board[0][1] == board[1][1] and board[1][1] == board[2][1]:
+        return board[0][1]
+    elif board[0][2] == board[1][2] and board[1][2] == board[2][2]:
+        return board[0][2]
+    elif board[0][0] == board[1][1] and board[1][1] == board[2][2]:
+        return board[0][0]
+    elif board[0][2] == board[1][1] and board[1][1] == board[0][2]:
+        return board[0][0]
 
+
+def winner(count):
+    if count < 5:
+        return
+    elif count == 9:
+        return "draw"
+    else:
+        return looking_at_the_board()
 
 print("      Hello welcome to The Tic Tac Toe Game!")
 print("-------Who will be the Winner in this game-------")
 print("-------------------------------------------------")
 
 while running:
+    turn_count = 0
     user_start = input("Do you want to start the game? y or n? ")
     if user_start == "n":
         break
     else:
+        game_running = True
         print("X will start the game off and will be player one!")
         while game_running:
             correct = False
             while not correct:
-                user_answer = input("Enter in corrdinates: (example: 1 1 is the top left and 3 3 is the bottom right)")
+                user_answer = input("Enter in coordinates: (example: 1 1 is the top left and 3 3 is the bottom right)")
                 try:
                     answer = user_answer.split(' ')
                     row = int(answer[0]) - 1
@@ -62,17 +88,17 @@ while running:
                     #print(row in range(3))
                     if row in range(3):
                         if col in range(3):
-                            print("in 2d array")
+                            #print("in 2d array")
                             if board[row][col] == "*":
-                                print("made it into the equals *")
+                                #print("made it into the equals *")
                                 board[row][col] = player_turn(player)
-                                print("Should've changed the board")
+                                #print("Should've changed the board")
                                 if player == 1:
                                     player = 2
                                 else:
                                     player = 1
                                 correct = True
-                                count = count+1
+                                turn_count = turn_count+1
                             else:
                                 print("Try again!")
                         else:
@@ -82,3 +108,13 @@ while running:
                 except:
                     print("Try again!")
             print_board()
+            winner_is = winner(turn_count)
+            if winner_is == "X":
+                print("Player 1 is the Winner! ha player 2 lost!")
+                game_running = False
+            elif winner_is == "O":
+                print("Player 2 is the Winner! ha player 1 lost!")
+                game_running = False
+            elif winner_is == "draw":
+                print("The game is a draw! Try again")
+                game_running = False
